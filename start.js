802e1c70191d057975925b8c5c66ccbcb0d53b37
hlecuyer <hugo.lecuyer1@gmail.com>
2017-02-13 08:55:58 +0100

    start

---
+++ start.js
@@ -0,0 +1,22 @@
+/**
+ * Created by hugo on 11/02/17.
+ */
+console.log('Hello, world!');
+var program = require('commander');
+var co = require('co');
+var prompt = require('co-prompt');
+
+
+
+program
+    .arguments('<file>')
+    .option('-u, --username <username>', 'The user to authenticate as')
+    .option('-p, --password <password>', 'The user\'s password')
+    .action(function(file) {
+        co(function *() {
+        var username = yield prompt('username: ');
+        var password = yield prompt.password('password: ');
+            console.log('user: %s pass: %s file: %s', username, password, file);
+        });
+    })
+    .parse(process.argv);
